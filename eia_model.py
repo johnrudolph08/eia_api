8bfcd35f45bb011eb485d4a6e2d53946b5049b54
John Rudolph <john.rudolph08@gmail.com>
2016-07-16 18:32:29 -0400

    working version of eia_api object with attributes set based on json parms

--- eia_model.py
+++ eia_model.py
@@ -2,66 +2,52 @@
 import requests
 import json
 
-
-class SeriesQuery:
+class CallApi:
+    """
+    Class the returns an eia_api object
+    Attributes are set for each json item and can be returned by
+    calling CallApi.item_item2 EX: CallApi.series or CallApi.series_data for nested item
+    """
 
     eia_url = 'http://api.eia.gov/series/'
 
     def __init__(self, api_key, series_id):
         """
-        Require and api_key and series_id
-
+        Create eia_api object and related attriobutes from json
         :param api_key: an API key that is provided by EIA
         :param series_id: The series id (also called source key) is a
                        case-insensitive string consisting of letters, numbers, dashes
                        ("-") and periods (".") that uniquely identifies an EIA series
         """
-        self.api_parms = (
-            ('api_key', api_key),
-            ('series_id', series_id),
+        self.api_key = api_key
+        self.series_id = series_id
+        self.json = self.get_series()
+        self.create_atttributes()
+
+    def get_series(self):
+        """
+        Calls the EIA API with supplied api_key on init and series_id and return json
+        """
+        api_parms = (
+            ('api_key', self.api_key),
+            ('series_id', self.series_id),
         )
-
-        self.json = self.get_json()
-
-    def get_json(self):
-        """
-        Makes a call to the EIA API and returns a dictionary from JSON
-        """
-        eia_req = requests.get(self.eia_url, params=self.api_parms)
+        eia_req = requests.get(self.eia_url, params=api_parms)
         return json.loads(eia_req.text)
 
-    def get_field(self, field):
+    def create_atttributes(self):
         """
-        Takes a dict with nested lists and dicts,
-        and searches all dicts for a key of the field
-        provided.
-
-        :param field: A field to search for in search_dict
+        Set class attributes for each dict item from returned json
         """
-        fields_found = []
-
         for key, value in self.json.items():
-
-            if key == field:
-                fields_found.append(value)
-
-            elif isinstance(value, dict):
-                results = self.get_field(value, field)
-                for result in results:
-                    fields_found.append(result)
-
+            # to handle request key of JSON that returns a nested dictionary
+            if isinstance(value, dict):
+                setattr(self, key, value)
+                for key2, value2 in value.items():
+                    setattr(self, key + "_" + key2, value2)
+            # to handle series key of JSON that returns a list with nested dictionary
             elif isinstance(value, list):
-                for item in value:
-                    if isinstance(item, dict):
-                        more_results = self.get_field(item, field)
-                        for another_result in more_results:
-                            fields_found.append(another_result)
-
-        return fields_found
-
-
-# def eia_data_series_to_df(eia_series):
-#     '''Converts eia api data series to dataframe'''
-#     eia_df = pd.DataFrame(eia_series)
-#     eia_df.columns = ['date', 'value']
-#     return eia_df
+                #get nested dictionary from first item in list
+                setattr(self, key, value[0])
+                for key2, value2 in value[0].items():
+                    setattr(self, key + "_" + key2, value2)
