ce349447e474866f73f3aa036f7237409d4c3897
John Rudolph <john.rudolph08@gmail.com>
2016-07-26 17:02:57 -0400

    updated api call to pull multiple series

--- eia_model.py
+++ eia_model.py
@@ -1,6 +1,7 @@
-# import pandas as pd
 import requests
 import json
+import datetime
+
 
 class CallApi:
     """
@@ -11,16 +12,17 @@ class CallApi:
 
     eia_url = 'http://api.eia.gov/series/'
 
-    def __init__(self, api_key, series_id):
+    def __init__(self, api_key, *args):
         """
         Create eia_api object and related attriobutes from json
         :param api_key: an API key that is provided by EIA
-        :param series_id: The series id (also called source key) is a
+        :param *args: The series id (also called source key) is a
                        case-insensitive string consisting of letters, numbers, dashes
-                       ("-") and periods (".") that uniquely identifies an EIA series
+                       ("-") and periods (".") that uniquely identifies an EIA series\
+                       multiple series can be submitted by comma separation ex: api_key, s1, s2
         """
         self.api_key = api_key
-        self.series_id = series_id
+        self.series_id = [";".join(args)]
         self.json = self.get_series()
         self.create_atttributes()
 
@@ -43,11 +45,26 @@ class CallApi:
             # to handle request key of JSON that returns a nested dictionary
             if isinstance(value, dict):
                 setattr(self, key, value)
-                for key2, value2 in value.items():
-                    setattr(self, key + "_" + key2, value2)
-            # to handle series key of JSON that returns a list with nested dictionary
+            # to handle series key of JSON that returns a list with nested
             elif isinstance(value, list):
-                #get nested dictionary from first item in list
-                setattr(self, key, value[0])
-                for key2, value2 in value[0].items():
-                    setattr(self, key + "_" + key2, value2)
+                for i, item in enumerate(value):
+                    setattr(self, 'series_{}'.format(i+1), item)
+
+
+def create_datetime(series):
+        """
+        Creates a list of datetimes from eia_json
+        :param series is an eia series list of attributes
+        """
+        freq = {'A': '%Y', 'M': '%Y%m', 'W': '%Y%m%d', 'D': '%Y%m%d'}
+        date_list = [datetime.datetime.strptime(x[0], freq[series['f']]).strftime(
+            '%Y-%m-%d %H:%M:%S') for x in series['data']]
+        return date_list
+
+
+def create_values(series):
+        """
+        Creates a list of values
+        :param series is an eia series list of attributes
+        """
+        return [x[1] for x in series['data']]
