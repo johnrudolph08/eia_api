75760c5f9006f3c89585dfc2c6e6044b9d450721
John Rudolph <john.rudolph08@gmail.com>
2016-07-31 23:26:35 -0400

    updated to include linear interpolation

--- eia_model.py
+++ eia_model.py
@@ -2,6 +2,8 @@ import requests
 import json
 import datetime
 import pyowm
+import numpy as np
+from scipy.interpolate import interp1d
 
 
 class CallApi:
@@ -54,42 +56,54 @@ class GetWeather:
 
     def __init__(self, api_key):
         self.api_key = api_key
-        self.wether_data = self.get_weather_data()
-        self.temps_dict = self.get_temperature(self.wether_data)
-        self.temps = self.temps_dict['temp']
-        self.max_temps = self.temps_dict['temp_max']
-        self.min_temps = self.temps_dict['temp_min']
-        self.time = self.get_time(self.wether_data)
+        self.wether = self.get_weather_data('Seattle,US')
+        self.weather_detail = self.get_weather_detail(self.wether)
+        self.time_1hr = self.get_time_1hr(self.weather_detail)
+        self.temps = self.interpolate_weather(self.weather_detail, 'temp')
 
-    def get_weather_data(self):
+    def get_weather_data(self, location):
         """
         Gets weather forecast from openweathermap
         """
         owm = pyowm.OWM(self.api_key)
-        fc = owm.three_hours_forecast('Seattle,US')
+        fc = owm.three_hours_forecast(location)
         f = fc.get_forecast()
         return f.get_weathers()
 
-    def get_temperature(self, forecast):
+    def get_weather_detail(self, weather):
         """
-        Parses temperature and time from weather objects imbedded in forecast object
-        :forecast object returned from get_weather_data
+        Returns a nested dictionary where first key is date and second key is temp attrs
+        :param weather is a object from get_weather_data
         """
-        temps_dict = {'temp': None, 'temp_max': None, 'temp_min': None}
-        for key in temps_dict:
-            temps_dict[key] = [i.get_temperature(
-                'fahrenheit').get(key) for i in forecast]
-        return temps_dict
+        time_dict = {}
+        # first loop through each 3 hr interval in forecast
+        for i in weather:
+            time = datetime.datetime.fromtimestamp(
+                i.get_reference_time()).strftime('%Y-%m-%d %H:%M:%S')
+            temps_dict = {'temp': None, 'temp_max': None, 'temp_min': None}
+            # create nested dict for each temp attribute
+            for j in temps_dict:
+                temps_dict[j] = i.get_temperature('fahrenheit').get(j)
+            time_dict[time] = temps_dict
 
-    def get_time(self, forecast):
+        return time_dict
+
+    def interpolate_weather(self, weather_detail, key):
+        temps = np.asarray([weather_detail[i][key] for i in weather_detail])
+        x = np.linspace(1, len(temps), num=len(temps), endpoint=True)
+        f_cubic = interp1d(x, temps, kind='cubic')
+        temp_int = f_cubic(np.linspace(1, len(temps), num=len(temps) * 3, endpoint=True))
+        return temp_int.tolist()
+
+    def get_time_1hr(self, weather_detail):
         """
         Parses temperature and time from weather objects imbedded in forecast object
-        :forecast object returned from get_weather_data
+        :param weather detail is dictionary returned from get_weather_detail
         """
-        time_3hr = [datetime.datetime.fromtimestamp(i.get_reference_time()).strftime('%Y-%m-%d %H:%M:%S')
-                    for i in forecast]
-        time1 = datetime.datetime.strptime(time_3hr[0], '%Y-%m-%d %H:%M:%S')
-        time2 = datetime.datetime.strptime(time_3hr[-1], '%Y-%m-%d %H:%M:%S')
+        time1 = datetime.datetime.strptime(
+            min(weather_detail), '%Y-%m-%d %H:%M:%S')
+        time2 = datetime.datetime.strptime(
+            max(weather_detail), '%Y-%m-%d %H:%M:%S')
         dif = int((time2 - time1).total_seconds() / 3600)
         time_1hr = [(time1 + datetime.timedelta(hours=x)).strftime('%Y-%m-%d %H:%M:%S')
                     for x in range(dif + 1)]
