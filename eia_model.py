35da6315403e0da2ce38bf72c9637eb510d733fb
John Rudolph <john.rudolph08@gmail.com>
2016-08-06 01:56:54 -0400

    added GetWeather to transform 3 hour to 1 hour using pandas

--- eia_model.py
+++ eia_model.py
@@ -52,6 +52,7 @@ class GetEnergy(object):
         """Parse dates from eia json['series']
         :param series: a series object returned by eia json['series']
         """
+        # create a dict to look up datetime frequency values
         freq = {'A': '%Y', 'M': '%Y%m', 'W': '%Y%m%d', 'D': '%Y%m%d'}
         date_list = [datetime.strptime(x[0], freq[series['f']]).strftime(
             '%Y-%m-%d %H:%M:%S') for x in series['data']]
@@ -69,21 +70,23 @@ class GetWeather:
 
     def __init__(self, api_key):
         self.api_key = api_key
-        self.wether = self.get_weather_data('Seattle,US')
-        self.weather_detail = self.get_weather_detail(self.wether)
+        self.weather = self.get_weather_data('Seattle,US')
+        self.weather_detail = self.get_weather_detail(self.weather)
         self.time_1hr = self.get_time_1hr(self.weather_detail)
-        self.temps = self.interpolate_weather(self.weather_detail, 'temp')
+        # self.temps = self.interpolate_weather(self.weather_detail, 'temp')
 
     def get_weather_data(self, location):
         """
         Gets weather forecast from openweathermap
+        :param location: a valid location provided to owm weather api
         """
         owm = pyowm.OWM(self.api_key)
         fc = owm.three_hours_forecast(location)
         f = fc.get_forecast()
         return f.get_weathers()
 
-    def get_weather_detail(self, weather):
+    @staticmethod
+    def get_weather_detail(weather):
         """
         Returns a nested dictionary where first key is date and second key is temp attrs
         :param weather is a object from get_weather_data
@@ -91,60 +94,36 @@ class GetWeather:
         time_dict = {}
         # first loop through each 3 hr interval in forecast
         for i in weather:
-            time = datetime.datetime.fromtimestamp(
+            time = datetime.fromtimestamp(
                 i.get_reference_time()).strftime('%Y-%m-%d %H:%M:%S')
             temps_dict = {'temp': None, 'temp_max': None, 'temp_min': None}
             # create nested dict for each temp attribute
             for j in temps_dict:
                 temps_dict[j] = i.get_temperature('fahrenheit').get(j)
             time_dict[time] = temps_dict
-        return time_dict
+        return pd.DataFrame.from_dict(time_dict, orient='index')
 
-    def interpolate_weather(self, weather_detail, key):
-        """
-        Interpolates the 3hr forecast to an hourly forecast using cubic interpolation
-        :param weather_detail is dict object of weather attributes returned from get_weather_detail
-        :param key is dict key to return temperature (temp, temp_max or temp_min)
-        """
-        # TODO need to sort weather detail by date !!!arrgrggg
-        temps = np.asarray([weather_detail[i][key] for i in weather_detail])
-        x = np.linspace(1, len(temps), num=len(temps), endpoint=True)
-        # create cubic interpolation object based on 3 hr temp forecast
-        f_cubic = interp1d(x, temps)
-        # apply cubic interpolation to hourly observations
-        temp_int = f_cubic(np.linspace(
-            1, len(temps), num=len(temps) * 3, endpoint=True))
-        return temp_int.tolist()
-
-    def get_time_1hr(self, weather_detail):
+    #TODO NEED TO INTERPOLATE
+    # def interpolate_weather(self, weather_detail, key):
+    #     """
+    #     Interpolates the 3hr forecast to an hourly forecast using cubic interpolation
+    #     :param weather_detail is dict object of weather attributes returned from get_weather_detail
+    #     :param key is dict key to return temperature (temp, temp_max or temp_min)
+    #     """
+    #     # TODO need to sort weather detail by date !!!arrgrggg
+    #     temps = np.asarray([weather_detail[i][key] for i in weather_detail])
+    #     x = np.linspace(1, len(temps), num=len(temps), endpoint=True)
+    #     # create cubic interpolation object based on 3 hr temp forecast
+    #     f_cubic = interp1d(x, temps)
+    #     # apply cubic interpolation to hourly observations
+    #     temp_int = f_cubic(np.linspace(
+    #         1, len(temps), num=len(temps) * 3, endpoint=True))
+    #     return temp_int.tolist()
+    @staticmethod
+    def get_time_1hr(weather_detail):
         """
         Parses temperature and time from weather objects imbedded in forecast object
         :param weather detail is dictionary returned from get_weather_detail
         """
-        time1 = datetime.strptime(
-            min(weather_detail), '%Y-%m-%d %H:%M:%S')
-        time2 = datetime.strptime(
-            max(weather_detail), '%Y-%m-%d %H:%M:%S')
-        dif = int((time2 - time1).total_seconds() / 3600)
-        time_1hr = [(time1 + datetime.timedelta(hours=x)).strftime('%Y-%m-%d %H:%M:%S')
-                    for x in range(dif + 1)]
-        return time_1hr
-
-
-def create_datetime(series):
-    """
-    Creates a list of datetimes from eia_json
-    :param series is an eia series list of attributes
-    """
-    freq = {'A': '%Y', 'M': '%Y%m', 'W': '%Y%m%d', 'D': '%Y%m%d'}
-    date_list = [datetime.strptime(x[0], freq[series['f']]).strftime(
-        '%Y-%m-%d %H:%M:%S') for x in series['data']]
-    return date_list
-
-
-def create_values(series):
-    """
-    Creates a list of values
-    :param series is an eia series list of attributes
-    """
-    return [x[1] for x in series['data']]
+        min_time, max_time = weather_detail.index.min(), weather_detail.index.max()
+        return pd.date_range(min_time, max_time, freq='H')
